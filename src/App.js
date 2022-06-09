8a02b449e2aa7168c68e2ca6ac39387478955668
Ezequiel Godoy <ezegodoy.eg@gmail.com>
2022-06-09 16:35:40 -0300

    desafio 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './favicon.webp';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tienda Ser Impersonal
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/ser.impersonal/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visita nuestro Instagram
         </a>
       </header>
     </div>
---
+++ src/Caja.js
@@ -0,0 +1,14 @@
+
+
+
+function Caja(props) {
+    return (
+      <div className="Caja">
+       <p>
+        Bienvenido a la Tienda {props.nombre}!
+       </p>
+      </div>
+    );
+  }
+  
+  export default Caja;
